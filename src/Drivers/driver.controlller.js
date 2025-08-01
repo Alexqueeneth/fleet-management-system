8629ff666e21e48f269dd1487a2f0e5fddbd30dd
David <nwokorochukwudi2001@gmail.com>
2025-08-01 06:30:31 -0700

    update

--- src/Drivers/driver.controlller.js
+++ src/Drivers/driver.controlller.js
@@ -1,2 +1,35 @@
 import * as driverService from "./driver.service.js";
 import { catchAsync, sendResponse } from '../common/utils.common.js';
+
+export const getAllDrivers = catchAsync(async (req, res) =>
+     {
+        const drivers = driverService.getAllDrivers();
+        sendResponse(res, 200, true, "fetched all drivers", drivers);
+        });
+export const getDriverById = catchAsync(async (req, res) => 
+    {
+        const { id } = req.params;
+        const driver = driverService.getDriverById(id);
+
+        sendResponse(res, 200, true, "Fetched driver by ID", driver);
+    })
+
+export const createDriver = catchAsync(async (req, res) =>
+    {
+        const driver = driverService.createDriver(req.body);
+        sendResponse(res, 201, true, "Driver created successfully", driver);
+    })
+
+export const updateDriver = catchAsync(async (req, res) =>
+    {
+        const { id } = req.params;
+        const updatedDriver = driverService.updateDriver(id, req.body);
+        sendResponse(res, 200,true, "Driver updated successfully", updatedDriver);
+    })
+export const deleteDriver = catchAsync(async (req, res) => 
+    {
+        const { id } = req.params;
+        driverService.deleteDriver(id);
+
+        sendResponse(res, 200, true, "Driver deleted successfully", null);
+    })
--- src/Drivers/driver.service.js
+++ src/Drivers/driver.service.js
@@ -1,1 +1,27 @@
 import { Vehicle } from "../vehicles/vehicles.models";
+
+export const getDrivers =  async () => {
+    return await Driver.findAll();
+}
+
+export const getDriverById = async (id) => {
+
+    return await Driver.findByPk(id);
+}
+
+
+export const createDriver =  async (Driver) => {
+    const newDriver = await Driver.create(Driver)
+
+     return newDriver;
+}
+
+export const updateDriver = async (id, Driver) => {
+    const updatedDriver = await Driver.update(Driver, {where:{id}})
+     return updatedDriver
+}
+
+export const deleteDriver = async (id) => {
+    await Driver.destroy({where:{id}})
+    return true
+}
